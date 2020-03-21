a0a178b0b3acb9cc33788b68aadab433b75eca1d
mashudiogan <anangsatria61@gmail.com>
2020-03-21 21:41:27 +0700

    Artikel Cuaca

---
+++ Artikel.py
@@ -0,0 +1,7 @@
+Jakarta memasuki akhir pekan diperkirakan cerah berawan pagi ini. Langit kembali berawan pada siang hari, namun sebagian bakal diguyur hujan lokal. 
+
+Kondisi cuaca hari ini, Sabtu (21/3/2020) diungkap Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) lewat laman resminya. 
+
+"Waspada potensi hujan disertai kilat/petir dan angin kencang dengan durasi singkat di wilayah Jaksel pada sore hari," tulis BMKG. 
+
+Cuaca cerah berawan juga dilaporkan akan menyinari tiga kota penyangga Jakarta pagi hari ini. Siang hingga malam hari, Bekasi dan Depok berawan, sementara Bogor dan Tangerang hujan lokal dan ringan.
